deae02fd8fddec31de4a5429fd91ee593839c4cc
Eric So <soer@oregonstate.edu>
2026-04-30 22:37:17 -0500

    Update leds.rs
    corrected the logic all referring to led1
    
    Signed-off-by: Eric So <soer@oregonstate.edu>

--- humidity-sensor/src/leds.rs
+++ humidity-sensor/src/leds.rs
@@ -14,9 +14,9 @@ pub fn update_leds(
   led6: &mut Output<'static>,
 ) {
   if humidity >= 20.0 {led1.set_high();} else {led1.set_low();}
-  if humidity >= 30.0 {led2.set_high();} else {led1.set_low();}
-  if humidity >= 40.0 {led3.set_high();} else {led1.set_low();}
-  if humidity >= 50.0 {led4.set_high();} else {led1.set_low();}
-  if humidity >= 60.0 {led5.set_high();} else {led1.set_low();}
-  if humidity >= 70.0 {led6.set_high();} else {led1.set_low();}
+  if humidity >= 30.0 {led2.set_high();} else {led2.set_low();}
+  if humidity >= 40.0 {led3.set_high();} else {led3.set_low();}
+  if humidity >= 50.0 {led4.set_high();} else {led4.set_low();}
+  if humidity >= 60.0 {led5.set_high();} else {led5.set_low();}
+  if humidity >= 70.0 {led6.set_high();} else {led6.set_low();}
 }
